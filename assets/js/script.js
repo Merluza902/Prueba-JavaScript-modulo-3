d45a80177116bea97b21be8982a9a101c8f045cf
merluzza <melisadiaz1234@gmail.com>
2024-05-16 13:12:04 -0400

    Funcion validar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-    
+    $("#form").on("submit", function(e){
+        //Captura el valor que ingresa el usuario
+        let number = parseInt($("#idHero").val())
+        e.preventDefault();
+        //Llamado función para que se ejecute
+        validar(number);
+    });
+
+
+    //FUNCIÓN PARA VALIDAR QUE EL DATO INGRESADO SEA UN NÚMERO
+    function validar(num) {
+        let exp = /^[0-9]+$/;
+        if (exp.test(num)) {
+            console.log("Numero ingresado por el usuario: ",num);
+        }else{
+            alert("INGRESA UN NÚMERO VÁLIDO");
+        }
+    }
 })
