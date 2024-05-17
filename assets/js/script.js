e491d186db5fa34f4d748fbac742a26655028284
merluzza <melisadiaz1234@gmail.com>
2024-05-16 22:33:35 -0400

    Corrección función

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,71 +34,76 @@ $(document).ready(function(){
             method: "GET",
             url: apiUrl,
             success: function(respuesta){
+                if (respuesta.response == "success") {
+                    let heroe =`
+                    <h3>SuperHeroe encontrado</h3>
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="${respuesta.image.url}" class="card-img" alt=""></img>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                <h5 class="card-title">Nombre: ${respuesta.name}</h5>
+                                <p class="card-text">Conexiones: ${respuesta.connections["group-affiliation"]}</p>
+                                <ul class="list-group">
+                                <li class="list-group-item">
+                                <em>Publicado por: ${respuesta.biography.publisher}</em>
+                                </li>
+                                <li class="list-group-item">
+                                <em>Ocupación: ${respuesta.work.occupation}</em>
+                                </li>
+                                <li class="list-group-item">
+                                <em>Primera aparición: ${respuesta.biography["first-appearance"]}</em>
+                                </li>
+                                <li class="list-group-item">
+                                <em>Altura: ${respuesta.appearance.height.join("-")}</em>
+                                </li>
+                                <li class="list-group-item">
+                                <em>Peso: ${respuesta.appearance.weight.join("-")}</em>
+                                </li>
+                                <li class="list-group-item">
+                                <em>Aliases: ${respuesta.biography.aliases}</em>
+                                </li>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    `
+    
+                    $("#resultado").append(heroe)
+    
+                    //Cargar estadisticas
+                    let datos = []
+                    for(let k in respuesta.powerstats){
+                        datos.push({x: k, y: respuesta.powerstats[k]})
+                    }
+    
+                    let options ={
+                        title: {
+                            text: `Estadísticas de poder ${respuesta.name}`,
+                        },
+                        data: [
+                            {
+                                type: "pie",
+                                startAngle: 45,
+                                showInLegend: "true",
+                                legendText: "{label}",
+                                indexLabel: "{label} ({y})",
+                                yValueFormatString: "#, ##0.#" % "",
+                                dataPoints: datos,
+                            },
+                        ],
+                    };
+                    $("#chartContainer").CanvasJSChart(options);
+                }else{
+                    alert("HÉROE NO ENCONTRADO")
+                }
 
                 //Muestra la data por consola
                 console.log("Esta es la data ",respuesta);
 
-                let heroe =`
-                <h3>SuperHeroe encontrado</h3>
-                <div class="card">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="${respuesta.image.url}" class="card-img" alt=""></img>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                            <h5 class="card-title">Nombre: ${respuesta.name}</h5>
-                            <p class="card-text">Conexiones: ${respuesta.connections["group-affiliation"]}</p>
-                            <ul class="list-group">
-                            <li class="list-group-item">
-                            <em>Publicado por: ${respuesta.biography.publisher}</em>
-                            </li>
-                            <li class="list-group-item">
-                            <em>Ocupación: ${respuesta.work.occupation}</em>
-                            </li>
-                            <li class="list-group-item">
-                            <em>Primera aparición: ${respuesta.biography["first-appearance"]}</em>
-                            </li>
-                            <li class="list-group-item">
-                            <em>Altura: ${respuesta.appearance.height.join("-")}</em>
-                            </li>
-                            <li class="list-group-item">
-                            <em>Peso: ${respuesta.appearance.weight.join("-")}</em>
-                            </li>
-                            <li class="list-group-item">
-                            <em>Aliases: ${respuesta.biography.aliases}</em>
-                            </li>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                `
 
-                $("#resultado").append(heroe)
-
-                //Cargar estadisticas
-                let datos = []
-                for(let k in respuesta.powerstats){
-                    datos.push({x: k, y: respuesta.powerstats[k]})
-                }
-
-                let options ={
-                    title: {
-                        text: `Estadísticas de poder ${respuesta.name}`,
-                    },
-                    data: [
-                        {
-                            type: "pie",
-                            startAngle: 45,
-                            showInLegend: "true",
-                            legendText: "{label}",
-                            indexLabel: "{label} ({y})",
-                            yValueFormatString: "#, ##0.#" % "",
-                            dataPoints: datos,
-                        },
-                    ],
-                };
-                $("#chartContainer").CanvasJSChart(options);
 
             },
 
